541a818745c1b041439a6b4aae609fbdd9b0ff9d
bobekriebel507 <bobekriebel507@gmail.com>
2025-03-13 11:44:16 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,11 @@
+import random
+
+def random_python_code():
+    # Generate a random number between 1 and 10
+    x = random.randint(1, 10)
+
+    # Print the result
+    print("The result is:", x)
+
+# Call the function
+random_python_code()
