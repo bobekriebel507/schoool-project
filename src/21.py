e8e46cadada4548736ee0f8844a0bcfb850470b3
bobekriebel507 <bobekriebel507@gmail.com>
2025-03-31 17:50:57 +0000

    Add 21.py

---
+++ src/21.py
@@ -0,0 +1,15 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+    return sequence
+
+print(fibonacci_sequence(10))
