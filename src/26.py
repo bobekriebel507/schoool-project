0f5a43327bd323cb039993e6df591ee4e47f21f4
bobekriebel507 <bobekriebel507@gmail.com>
2025-04-06 00:22:06 +0000

    Add 26.py

---
+++ src/26.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Please enter a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, n):
+            next_value = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_value)
+        return fib_sequence
