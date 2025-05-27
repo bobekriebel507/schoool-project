2ba93793b00edbba36d8b393e6b2e945459fe46f
bobekriebel507 <bobekriebel507@gmail.com>
2025-05-27 12:02:12 +0000

    Add 44.py

---
+++ src/44.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+def multiply_numbers(a: int, b: int) -> int:
+    return a * b
+
+def subtract_numbers(a: int, b: int) -> int:
+    return a - b
